3ec3c61ebc4e02882544a765cb2c4894577425ed
mohan.kumar2 <mohan.kumar2@iqvia.com>
2026-09-16 20:06:06 +0530

    aidlc: update 2 file(s)

--- app/main.py
+++ app/main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/hello_hi")
-def hello_hi():
-    return {"message": "Hello, Hi!"}
+@app.get("/hello_vanakam")
+async def hello_vanakam():
+    return {"message": "Vanakam!"}
