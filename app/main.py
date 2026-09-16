93c87d88c3df292309d2720cf20ac7800369f7f9
mohan.kumar2 <mohan.kumar2@iqvia.com>
2026-09-16 23:54:46 +0530

    aidlc: update 2 file(s)

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/hello_hi")
-def hello_hi():
-    return {"message": "Hello, Hi!"}
+@app.get("/hello_hi", response_model=dict)
+async def hello_hi():
+    return {"message": "Hello, hi!"}
+
+# Existing routes can be added below this line
